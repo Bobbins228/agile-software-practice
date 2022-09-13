128e46423b7205b53cf65168164dac7dbaf7c4bc
Bobbins228 <markcampbell114@gmail.com>
2022-09-13 10:37:05 +0100

    Arrays.

--- lab01/fundamentals.js
+++ lab01/fundamentals.js
@@ -1,5 +1,16 @@
 const aCar = { 
     owner : "Joe Bloggs", 
+    address: "3 Walkers Lane",
+    previous_owners: [
+        {
+            name: "Pat Smith",
+            address: "1 Main Street"
+        },
+        {
+            name: "Sheila Dwyer",
+            address: "2 High Street"
+        }
+    ],
     type : {
         make: 'Toyota',
         model: 'Corolla',
@@ -15,3 +26,5 @@ const aCar = {
 console.log(aCar.owner + " drives a " + aCar.type.make)
 
 console.log("Reg. = " + aCar.registration.year + "-" + aCar.registration.countryCode + "-" + aCar.registration.number)
+
+console.log('First owner : ' + aCar.previous_owners[0].name + " - " + aCar.previous_owners[0].address )
